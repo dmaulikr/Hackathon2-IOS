a83161ce1a418923dab22bbe42063aab93c0f399
nguyensonvu1102 <mrnguyensonvuads@gmail.com>
2016-05-29 09:36:05 +0700

    Start chinese chess

---
+++ Demo_Chinese_Chess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_Chinese_Chess
+//
+//  Created by Mr.Vu on 5/28/16.
+//  Copyright © 2016 Mr.Vu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo_Chinese_Chess/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo_Chinese_Chess
+//
+//  Created by Mr.Vu on 5/28/16.
+//  Copyright © 2016 Mr.Vu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
